ae9bd762246720614a2ebc3e0b8a34c21e1f502e
usama-jamil <usamajamil006@gmail.com>
2019-10-10 15:46:52 +0500

    socket counter

--- socket-chat/public/js/chat.js
+++ socket-chat/public/js/chat.js
@@ -1,1 +1,11 @@
-io()
+const socket = io();
+
+socket.on("countUpdated", (count) => {
+  console.log("The count has been Updated",count);
+});
+
+
+document.querySelector('#increment').addEventListener('click',()=>{
+    console.log('button clicked')
+    socket.emit('increment')
+})
--- socket-chat/src/index.js
+++ socket-chat/src/index.js
@@ -1,21 +1,32 @@
 const path = require("path");
-const http = require("http")
+const http = require("http");
 const express = require("express");
-const socketio = require("socket.io")
+const socketio = require("socket.io");
 
-const app =  express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-io.on("connection",()=>{
-    console.log('New Websocket connection')
-})
+let count = 0;
+io.on("connection", socket => {
+  console.log("New Websocket connection");
 
-server.listen(port,( ) =>{
-    console.log(`Server in Running on ${port}`)
-})
+  socket.emit("countUpdated", count);
+
+  socket.on("increment",()=>{
+      count++
+        //  Emit Event to Current connection
+      //   socket.emit("countUpdated",count)
+        //  Emit Event to Every Single Connection
+      io.emit("countUpdated",count)
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Server in Running on ${port}`);
+});
